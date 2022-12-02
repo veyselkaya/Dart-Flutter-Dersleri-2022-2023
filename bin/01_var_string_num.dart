6c888e11c9eccf3a0c19a48ee40b703aacdfe7fd
veyselkaya <veyselkaya03@gmail.com>
2022-12-02 15:08:14 +0300

    Main 01 add

---
+++ bin/01_var_string_num.dart
@@ -0,0 +1,16 @@
+void main() {
+  //Var:
+  //Var is a keyword that is used to declare a variable.
+  //Var is a dynamic type variable.
+  //Var is a variable that can be changed.
+
+  //String:
+  //String is a keyword that is used to declare a variable.
+  //String is a non-dynamic type variable.
+  //String is a variable that cannot be changed.
+
+  //num:
+  //num is a keyword that is used to declare a variable.
+  //num is a non-dynamic type variable.
+  //num is a variable that cannot be changed.
+}
